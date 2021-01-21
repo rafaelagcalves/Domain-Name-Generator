2f44b2384db87314c819af9b120c71f7d48ab371
Rafaela Gonçalves <rafaelagcalves@gmail.com>
2021-01-21 08:07:38 +0000

    Add files

--- src/main.js
+++ src/main.js
@@ -6,5 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["The", "Our", "Those"];
+  let adjs = ["great", "awesome", "dumb"];
+  let nouns = ["fri", "to", "pi"];
+  let extensions = [".es"];
+  let domain = "";
+  let domains = [];
+
+  for (let pronoun = 0; pronoun < pronouns.length; pronoun++) {
+    for (let adj = 0; adj < adjs.length; adj++) {
+      for (let noun = 0; noun < nouns.length; noun++) {
+        for (let extension = 0; extension < extensions.length; extension++) {
+          domain = domain.concat(
+            pronouns[pronoun],
+            adjs[adj],
+            nouns[noun],
+            extensions[extension]
+          );
+          domains.push(domain);
+          domain = "";
+        }
+      }
+    }
+  }
+
+  for (let domain of domains) {
+    let myDiv = document.querySelector("#myUl");
+    let myLi = document.createElement("li");
+    let t = document.createTextNode(domain);
+    myLi.appendChild(t);
+    myDiv.appendChild(myLi);
+  }
+
+  document.getElementById("myBody").style.backgroundImage =
+    "url(https://wallpaperaccess.com/full/2641047.gif)";
+
+  document.getElementById("myBody").style.color = "#000000";
 };
